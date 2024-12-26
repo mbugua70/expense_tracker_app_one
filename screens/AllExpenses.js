d92a61cd1b2ca1ae05211dd579a70b54f9ac598e
Code MBU <johnmbugua23code@gmail.com>
2024-12-26 18:32:22 +0300

    adding empty UI for all expenses page

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import Dashboard from '../components/Dashboard'
 import Expenses from '../components/Expenses'
 import HeaderExpenses from "../components/HeaderExpenses";
@@ -9,12 +9,24 @@ import { useContext } from "react";
 
 const AllExpenses = () => {
   const { expenseData } = useContext(ExpenseContext);
+
+  let content;
+
+  if(expenseData.length === 0){
+    content = (<View style={styles.rootScreen}>
+          <Image source={require("../assets/empty-box.png")} style={styles.image} />
+          <Text style={styles.favoriteText}>You have no expenses</Text>
+        </View>)
+  }else{
+    content=  <Expenses expenses={expenseData} />
+  }
   return (
     <View style={styles.screen}>
       <Dashboard expenses={expenseData} />
       {/* heading for expenses and button */}
       {/* <HeaderExpenses /> */}
-      <Expenses expenses={expenseData} />
+
+       {content}
       <AddNewExpense />
     </View>
   );
@@ -25,5 +37,25 @@ export default AllExpenses;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+  },
+
+  // styling For empty UI
+
+  rootScreen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  favoriteText: {
+    marginTop: 10,
+    color: "#000000",
+    textAlign: "center",
+    fontSize: 18,
+  },
+
+  image: {
+    width: 100,
+    height: 100,
   },
 });
